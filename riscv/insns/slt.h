9217c69adcf5888f6453c121a2780d017fe9b074
u1F98E <u1f98e@proton.me>
2025-01-29 18:58:56 +0000

    Add debug tag set/get instructions

--- riscv/insns/slt.h
+++ riscv/insns/slt.h
@@ -1,1 +1,10 @@
-WRITE_RD(sreg_t(RS1) < sreg_t(RS2));
+#ifdef TYPE_TAGGING_ENABLED
+if(sreg_t(RD) == 0 && p->get_tag_propagation_enabled()) {
+	// Debug instruction: store a tag
+	reg_t addr = RS1 + insn.s_imm();
+	raw_tag_t tag = RS2;
+	TAG_MMU.store<raw_tag_t>(addr, tag);
+}
+else
+#endif
+	WRITE_RD(sreg_t(RS1) < sreg_t(RS2));
