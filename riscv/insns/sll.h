dc52a29da1c0da1cfb8a8147684a55b11cf43ad2
cartui <cart8305@vandals.uidaho.edu>
2025-01-29 18:58:56 +0000

    Add debug tag set/get instructions

--- riscv/insns/sll.h
+++ riscv/insns/sll.h
@@ -1,1 +1,9 @@
-WRITE_RD(sext_xlen(RS1 << (RS2 & (xlen-1))));
+#ifdef TYPE_TAGGING_ENABLED
+if(sreg_t(RD) == 0 && p->get_tag_propagation_enabled()) {
+	// Debug instruction: get a tag
+	reg_t addr = RS1 + insn.s_imm();
+	WRITE_REG(insn.rs2(), TAG_MMU.load<raw_tag_t>(addr));
+}
+else
+#endif
+	WRITE_RD(sext_xlen(RS1 << (RS2 & (xlen-1))));
