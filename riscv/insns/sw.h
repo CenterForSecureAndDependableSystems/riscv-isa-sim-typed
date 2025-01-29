e2ab53821747901aae5157e1d6537f995b9dc6a2
u1F98E <u1f98e@proton.me>
2025-01-29 18:53:21 +0000

    Propagate a full word in sw/lw tag propagation

--- riscv/insns/sw.h
+++ riscv/insns/sw.h
@@ -1,7 +1,7 @@
 // MMU.store<uint32_t>(RS1 + insn.s_imm(), RS2);
-reg_t addr = RS1 + insn.s_imm();
-MMU.store<uint32_t>(addr, RS2);
+reg_t addr = READ_REG(insn.rs1()) + insn.s_imm();
+MMU.store<uint32_t>(addr, READ_REG(insn.rs2()));
 
 IF_TAG_PROPAGATION(
-	TAG_MMU.store<raw_tag_t>(addr, READ_REG_TAG(insn.rs2()));
+	TAG_MMU.store<uint32_t>(addr, READ_REG_TAG(insn.rs2()));
 )
