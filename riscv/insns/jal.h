24f570e72026ebdda6c2e7ef30807c145cf03dbd
cartui <cart8305@vandals.uidaho.edu>
2025-02-05 05:51:29 +0000

    Attempt to tag return addresses created by `jal`/`jalr` and check them upon `ret`

--- riscv/insns/jal.h
+++ riscv/insns/jal.h
@@ -2,3 +2,10 @@ CHECK_RD();
 reg_t tmp = npc;
 set_pc(JUMP_TARGET);
 WRITE_RD(tmp);
+
+// Tag return address 
+IF_TAG_PROPAGATION(
+	typetag_t tag = 0;
+	tag = tt_set_obj_type(tag, TT_OBJ_RETURN);
+	WRITE_REG_TAG(insn.rd(), tag);
+)
--- riscv/insns/jalr.h
+++ riscv/insns/jalr.h
@@ -1,9 +1,32 @@
 CHECK_RD();
+
+IF_TAG_CHECKING(
+    // If this is a `ret` pseudoinstruction (RD == x0)
+    // and the tag is not marked as a valid return address,
+    // panic.
+    if(insn.rd() == 0 
+       && tt_get_obj_type(READ_REG_TAG(insn.rs1())) != TT_OBJ_RETURN
+       && p->get_tag_fault_enabled(TT_EXP_INVALID_RETURN_TAG))
+    {
+        #ifdef TYPE_TAGGING_DEBUG
+        printf("[SPIKE DEBUG] FAULT: Return address was not tagged appropriately\n");
+        #endif
+        // throw trap_return_tag_fault();
+    }
+)
+
 reg_t tmp = npc;
 set_pc((RS1 + insn.i_imm()) & ~reg_t(1));
 WRITE_RD(tmp);
 
+// Tag return address 
+IF_TAG_PROPAGATION(
+	typetag_t tag = 0;
+	tag = tt_set_obj_type(tag, TT_OBJ_RETURN);
+	WRITE_REG_TAG(insn.rd(), tag);
+)
+
 if (ZICFILP_xLPE(STATE.v, STATE.prv)) {
-  STATE.elp = ZICFILP_IS_LP_EXPECTED(insn.rs1());
-  serialize();
+    STATE.elp = ZICFILP_IS_LP_EXPECTED(insn.rs1());
+    serialize();
 }
