1847b7407c088dd2e4c5d2a2727aa001daa014a2
u1F98E <u1f98e@proton.me>
2025-02-05 05:49:28 +0000

    Check the register instead of the value when interpreting tag control pseudoinstructions

--- riscv/insns/slt.h
+++ riscv/insns/slt.h
@@ -1,5 +1,5 @@
 #ifdef TYPE_TAGGING_ENABLED
-if(sreg_t(RD) == 0 && p->get_tag_propagation_enabled()) {
+if(insn.rd() == 0 && p->get_tag_propagation_enabled()) {
 	// Debug instruction: store a tag
 	reg_t addr = RS1 + insn.s_imm();
 	typetag_t tag = RS2;
