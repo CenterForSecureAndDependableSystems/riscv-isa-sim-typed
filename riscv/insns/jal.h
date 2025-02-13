ab5dfa251ee02b875169b8e865faf40da1a3c746
u1F98E <u1f98e@proton.me>
2025-02-13 03:22:41 +0000

    Ignore indirect jumps in jal for now

--- riscv/insns/jal.h
+++ riscv/insns/jal.h
@@ -4,7 +4,11 @@ set_pc(JUMP_TARGET);
 WRITE_RD(tmp);
 
 // Tag return address 
-IF_TAG_PROPAGATION(
-	typetag_t tag = tt_set_obj_type(0, TT_OBJ_RETURN);
-	WRITE_REG_TAG(insn.rd(), tag);
-)
+#ifdef TYPE_TAGGING_ENABLED
+if(p->get_tag_propagation_enabled()) {
+	// Ignore indirect jumps for now
+	if(insn.rd() != 0) {
+		WRITE_REG_TAG(insn.rd(), tt_set_obj_type(0, TT_OBJ_RETURN));
+	}
+}
+#endif
--- riscv/insns/jalr.h
+++ riscv/insns/jalr.h
@@ -1,24 +1,27 @@
 CHECK_RD();
 
-IF_TAG_CHECKING(
+#ifdef TYPE_TAGGING_ENABLED
+if(p->get_tag_checking_enabled()) {
     // If this is a `ret` pseudoinstruction (RD == x0)
     // and the tag is not marked as a valid return address,
     // panic.
-    if(insn.rd() == 0 && tt_get_obj_type(READ_REG_TAG(insn.rs1())) != TT_OBJ_RETURN) {
+    typetag_t tag = READ_REG_TAG(insn.rs1());
+    if(insn.rd() == 0 && tt_get_obj_type(tag) != TT_OBJ_RETURN) {
         TAG_TRAP(TT_EXP_INVALID_RETURN_TAG, "Return address was not tagged appropriately", trap_return_tag_fault());
     }
-)
+}
+#endif
 
 reg_t tmp = npc;
 set_pc((RS1 + insn.i_imm()) & ~reg_t(1));
 WRITE_RD(tmp);
 
 // Tag return address 
-IF_TAG_PROPAGATION(
-	typetag_t tag = 0;
-	tag = tt_set_obj_type(tag, TT_OBJ_RETURN);
-	WRITE_REG_TAG(insn.rd(), tag);
-)
+#ifdef TYPE_TAGGING_ENABLED
+if(p->get_tag_propagation_enabled()) {
+	WRITE_REG_TAG(insn.rd(), tt_set_obj_type(0, TT_OBJ_RETURN));
+}
+#endif
 
 if (ZICFILP_xLPE(STATE.v, STATE.prv)) {
     STATE.elp = ZICFILP_IS_LP_EXPECTED(insn.rs1());
